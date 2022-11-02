51f60cadaaab90acf2bd2e43694422743b33bc81
Steven Pak <spak9@gmu.edu>
2022-11-02 16:34:26 -0400

    ch2 finish

--- ch2/natter-api/src/main/java/com/manning/apisecurityinaction/Main.java
+++ ch2/natter-api/src/main/java/com/manning/apisecurityinaction/Main.java
@@ -2,7 +2,7 @@ package com.manning.apisecurityinaction;
 
 import java.nio.file.*;
 
-import com.manning.apisecurityinaction.SpaceController.*;
+import com.manning.apisecurityinaction.controller.*;
 import org.dalesbred.Database;
 import org.dalesbred.result.EmptyResultException;
 import org.h2.jdbcx.JdbcConnectionPool;
@@ -14,47 +14,79 @@ import static spark.Spark.*;
 public class Main {
 
     public static void main(String... args) throws Exception {
-        
-        // 1. Create privileged connection with default user to create database/tables
+
+        // Connect to our database source
         var datasource = JdbcConnectionPool.create(
             "jdbc:h2:mem:natter", "natter", "password");
         var database = Database.forDataSource(datasource);
+
+        // Create our SQL tables from "schema.sql"
         createTables(database);
 
-        // 2. Switch to our "natter_api_user" with fewer permissions (SELECT, INSERT)
-        System.out.println("[+] Switching to 'natter_api_user'.");
+        // After creation of tables, change user priv. so that only basic queries work
         datasource = JdbcConnectionPool.create(
             "jdbc:h2:mem:natter", "natter_api_user", "password");
         database = Database.forDataSource(datasource);
 
-        // Create our Controllers
+        // Before filters - check "POST" requests have correct Content-Type
+        before(((request, response) -> {
+            if (request.requestMethod().equals("POST") &&
+            !"application/json".equals(request.contentType())) {
+                halt(415, new JSONObject().put(
+                    "error", "Only application/json supported"
+                ).toString());
+            }
+        }));
+
+        /*
+            SpaceController and routes
+        */
         var spaceController = new SpaceController(database);
 
-        // Prepare our routes
         post("/spaces", spaceController::createSpace);
 
-        // after filters
-        after((request, response) -> {
-            response.type("application/json");
-        });
+        // Additional REST endpoints not covered in the book:
+        post("/spaces/:spaceId/messages", spaceController::postMessage);
+        get("/spaces/:spaceId/messages/:msgId", spaceController::readMessage);
+        get("/spaces/:spaceId/messages", spaceController::findMessages);
 
+        var moderatorController =
+            new ModeratorController(database);
+        delete("/spaces/:spaceId/messages/:msgId",
+            moderatorController::deletePost);
+
+        // Finally filters - put on HTTP response headers
         afterAfter((request, response) -> {
-            response.header("Server", "");
-
-            // Used to exemptlify XSS vuln.
+            response.type("application/json;charset=utf-8");
+            response.header("X-Content-Type-Options", "nosniff");
+            response.header("X-Frame-Options", "DENY");
             response.header("X-XSS-Protection", "0");
-            response.header("Content-Type", "text/html");
+            response.header("Cache-Control", "no-store");
+            response.header("Content-Security-Policy",
+                    "default-src 'none'; frame-ancestors 'none'; sandbox");
+            response.header("Server", "");
         });
 
-        internalServerError(new JSONObject().put("error", "internal server error").toString());
+        internalServerError(new JSONObject()
+            .put("error", "internal server error").toString());
+        notFound(new JSONObject()
+            .put("error", "not found").toString());
 
-        notFound(new JSONObject().put("error", "not found").toString());
-
+        exception(IllegalArgumentException.class, Main::badRequest);
+        exception(JSONException.class, Main::badRequest);
+        exception(EmptyResultException.class,
+            (e, request, response) -> response.status(404));
     }
 
+  private static void badRequest(Exception ex,
+      Request request, Response response) {
+    response.status(400);
+    response.body(new JSONObject().put("error", ex.getMessage()).toString());
+  }
+
     private static void createTables(Database database) throws Exception {
-        System.out.println("[+] createTables");
-        var path = Paths.get(Main.class.getResource("/schema.sql").toURI());
+        var path = Paths.get(
+                Main.class.getResource("/schema.sql").toURI());
         database.update(Files.readString(path));
     }
 }
--- ch2/natter-api/src/main/java/com/manning/apisecurityinaction/controller/SpaceController.java
+++ ch2/natter-api/src/main/java/com/manning/apisecurityinaction/controller/SpaceController.java
@@ -1,8 +1,14 @@
 package com.manning.apisecurityinaction;
 
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.stream.Collectors;
+
 import org.dalesbred.Database;
 import org.json.*;
+
 import spark.*;
+
 import java.sql.SQLException;
 
 /*
@@ -16,6 +22,12 @@ public class SpaceController {
 		this.database = database;
 	}
 
+	/*
+		POST /spaces
+
+		Create a space
+		{"name": str, "owner": str}
+	*/
 	public JSONObject createSpace(Request request, Response response) throws SQLException {
 		var json = new JSONObject(request.body());
 
@@ -53,4 +65,116 @@ public class SpaceController {
 				.put("uri", "/spaces/" + spaceId);
 		});
 	}
+
+
+	/*
+		POST /spaces/<spaceId>/messages
+
+		Create a message within <spaceId> space
+	*/
+	public JSONObject postMessage(Request request, Response response) throws SQLException {
+		var spaceId = Long.parseLong(request.params(":spaceId"));
+		var json = new JSONObject(request.body());
+
+		var user = json.getString("author");
+		if (!user.matches("[a-zA-Z][a-zA-Z0-9]{0,29}")) {
+			throw new IllegalArgumentException("invalid username");
+		}
+
+		var message = json.getString("message");
+		if (message.length() > 1024) {
+			throw new IllegalArgumentException("message is too long");
+		}
+
+		return database.withTransaction(tx -> {
+			var msgId = database.findUniqueLong("SELECT NEXT VALUE FOR msg_id_seq");
+
+			database.updateUnique(
+				"INSERT INTO messages(space_id, msg_id, msg_time, author, msg_text) " +
+				"VALUES(?, ?, current_timestamp, ?, ?)", 
+				spaceId, msgId, user, message);
+
+			// prepare response
+			response.status(201);
+			var uri = "/spaces/" + spaceId + "/messages/" + msgId;
+			response.header("Location", uri);
+			return new JSONObject().put("uri", uri);
+		});
+	}
+
+
+	/*
+		GET /spaces/<spaceId>/messages<messageId>
+
+		Return JSON with "message" data
+	*/
+	public Message readMessage(Request request, Response response) throws SQLException {
+		var spaceId = Long.parseLong(request.params(":spaceId"));
+		var messageId = Long.parseLong(request.params(":msgId"));
+
+		var message = database.findUnique(Message.class,
+			"SELECT space_id, msg_id, author, msg_time, msg_text " +
+			"FROM messages WHERE msg_id =? AND space_id = ?",
+			messageId, spaceId);
+
+		return message;
+	}
+
+	public JSONArray findMessages(Request request, Response response) {
+    var since = Instant.now().minus(1, ChronoUnit.DAYS);
+    if (request.queryParams("since") != null) {
+      since = Instant.parse(request.queryParams("since"));
+    }
+    var spaceId = Long.parseLong(request.params(":spaceId"));
+
+    var messages = database.findAll(Long.class,
+        "SELECT msg_id FROM messages " +
+            "WHERE space_id = ? AND msg_time >= ?;",
+        spaceId, since);
+
+    response.status(200);
+    return new JSONArray(messages.stream()
+        .map(msgId -> "/spaces/" + spaceId + "/messages/" + msgId)
+        .collect(Collectors.toList()));
+  }
+
+	// Message class
+	public static class Message {
+		private final long spaceId;
+		private final long msgId;
+		private final String author;
+		private final Instant time;
+		private final String message;
+
+		public Message(long spaceId, long msgId, String author,
+		    Instant time, String message) {
+		  this.spaceId = spaceId;
+		  this.msgId = msgId;
+		  this.author = author;
+		  this.time = time;
+		  this.message = message;
+		}
+
+		@Override
+		public String toString() {
+		  JSONObject msg = new JSONObject();
+		  msg.put("uri",
+		      "/spaces/" + spaceId + "/messages/" + msgId);
+		  msg.put("author", author);
+		  msg.put("time", time.toString());
+		  msg.put("message", message);
+		  return msg.toString();
+		}
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
